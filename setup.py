fc8fa1e3b6a9c9fa0c8ab23d707b7be92a746d63
q <lwzm@qq.com>
2017-03-07 10:28:34 +0800

    fix None error

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@
 import sys
 from setuptools import setup, find_packages
 
-setup(name="tornadospy",
-      packages=find_packages(),
-      package_data={"tornadospy": ["static/*", "templates/*"]},
-     )
-
+setup(
+    name="tornadospy",
+    version="1.0.1",
+    packages=find_packages(),
+    package_data={"tornadospy": ["static/*", "templates/*"]},
+)
